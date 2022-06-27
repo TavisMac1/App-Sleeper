0938f5c223b378da5642a36f9c8d77d4f6bb3ca5
Tavis <71576767+TavisMac1@users.noreply.github.com>
2022-06-27 10:36:31 -0300

    put open apps to sleep aside from ones predefined to be excluded and wake them up when CPU percentage goes down | will use user defined app names to be exluded later

--- main.py
+++ main.py
@@ -1,78 +1,67 @@
+import subprocess
+from ast import And
 from asyncio.windows_events import NULL
 from tabnanny import check
 import wmi
 import psutil
 winRef = wmi.WMI() #ref to WMI
 
-appToExlude = "Steam.exe" #which app should be excluded?
+appToExlude = "steam.exe" #which app should be excluded?
 allAvailableApps = [] #get all available apps
 appToMonitor = "" #app for us to monitor, when its network usage is normal, awaken apps
-suspendedApps = [] # apps that were suspended
-#user defined
-usrMmryUse = 0 #set a limiter for memory | cpu | network -> allow user defined for ram and cpu
-usrCpuUse = 0
-usrNetUse = 0 #hard code net use for now
-#predefined system vals
-mmryUse = 0  # set a limiter for memory | cpu | network -> allow user defined for ram and cpu
-cpuUse = 1
-netUse = 0  # hard code net use for now
+appsToSleep = [] # apps to sleep
 
-def killAllTmp(tmpToIgnore): # put apps to sleep aside from exluded apps
+cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Name'
+proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+
+def killAllTmp(): # put apps to sleep aside from exluded apps
+
+    for line in proc.stdout:
+        if line.rstrip():
+            if line.decode().rstrip() != appToExlude:
+                appsToSleep.append(line.decode().rstrip() + ".exe")
+
     for x in psutil.process_iter():
         try:
-            print(tmpToIgnore)
-            #break
-            processHigh = False
-            #print(processName, ' : ', processID)
+
             if x.name() == appToExlude:  # capture the app to monitor
                 appToMonitor = x
-                while appToMonitor.cpu_percent() > 5.0:
-                    print("cpu percent of steam higher than 5")
-                    for x in psutil.process_iter():
-                        if x.name() != tmpToIgnore:
-                            if x.cpu_percent() > cpuUse:
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            pass
+
+    while 1:
+        print("cpu percent of steam higher than 1")
+        for x in psutil.process_iter():
+
+            if x.name() != appToMonitor.name():
+                for z in appsToSleep:
+                    if x.name() == z:
+                        try:
+                            if x.cpu_percent(interval=0.1) > 0.1:
+                                print("putting apps to sleep")
                                 x.suspend()
-                                suspendedApps.append(x)
-                    if appToMonitor.cpu_percent() < 5.0:
-                        processHigh = True
-                    if processHigh == True:
-                        for y in suspendedApps:
-                            y.resume()
-                            print("trying to resume apps")
-                #print(appToMonitor + "MONITORED APP")
-            
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+                        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                            pass
 
-def checkStatus():
-    for x in psutil.process_iter():
-        try:
-            #print(x.name)
-            if x.name() == "Spotify.exe":  # capture the app to monitor
-                print(x.username())
-
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-checkStatus()
-
-def killSpotify():
-    for x in psutil.process_iter():
-        try:
-            if x.name() == "Spotify.exe":
-                x.suspend()
-                print("Spotify suspended")
-            else:
-                print("spotify not found")
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-#killSpotify()
-
+        if appToMonitor.cpu_percent(interval=0.1) < 1.0:
+           
+            for x in psutil.process_iter():
+                try:
+                    for i in appsToSleep:
+                        if x.name == i:
+                            print("trying to wake up apps")
+                            x.resume()
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    pass
+            break
+killAllTmp()
 
 def getNames(): #get names of apps -> for displaying to the user in GUI
     for x in psutil.process_iter():
         try:
             allAvailableApps.append(x.name())
-            #print(x.name())
+            print(x.name())
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-getNames()
+#getNames()
