219b05ae96487a20ef1cbf1c1f6aa18c0a26a8a1
Tavis <71576767+TavisMac1@users.noreply.github.com>
2022-06-27 10:43:55 -0300

    put open apps to sleep aside from ones predefined to be excluded and wake them up when CPU percentage goes down | will use user defined app names to be exluded later | changed position of GUI select box

--- mainGUI.py
+++ mainGUI.py
@@ -13,7 +13,7 @@ main.getNames()
 
 availableApps = main.allAvailableApps
 appSel = ttk.Combobox(window, values=availableApps, width=15)
-appSel.grid(column=2, row=0)
+appSel.grid(column=1, row=0)
 
 #sleepBtn = ttk.Button(window, text='Sleep', command=main.killAllTmp("Steam.exe"))
 #sleepBtn.grid(column=1, row=5)
